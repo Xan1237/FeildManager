2d2b09c8367f8a117d2cb3d7e9b807606a69b954
Xan1237 <154279844+Xan1237@users.noreply.github.com>
2024-12-01 23:42:22 -0400

    Moved folders
    added front end folders to public file

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 
-// Get __dirname equivalent in ES modules
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-/*connects to html page*/ 
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-/*enables the use of the css and javascript front end files*/
+
 app.use(express.static('./public'));
 
-/*starts the server at local port 8080*/
+
 app.listen(8080, () => {
     console.log("server is active");
 });
